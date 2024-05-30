3c8e1822f2b0f6ddc174339bdb2301cf81a0070d
premstaller1 <premstaller-spam@outlook.de>
2024-05-31 01:34:32 +0200

    cryptobert

--- streamlit-app.py
+++ streamlit-app.py
@@ -24,7 +24,7 @@ model_options = [
     "distilbert-base-uncased",
     "bert-base-uncased",
     "roberta-base",
-    "cryptobert"
+    "ElKulako/cryptobert"
 ]
 
 selected_model = st.selectbox("Select Hugging Face model", model_options)
