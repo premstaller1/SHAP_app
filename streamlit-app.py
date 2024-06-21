4c2e96ef7fe5b17b1a5209478ed96ca85912d2e2
premstaller1 <premstaller-spam@outlook.com>
2024-06-21 10:13:52 +0200

    test

--- streamlit-app.py
+++ streamlit-app.py
@@ -50,11 +50,11 @@ def load_model(model_name):
 @st.cache_data
 def display_shap_values(_shap_values, prediction):
     print("Displaying SHAP values...")
-         # Create a placeholder for the plot
+    # Create a placeholder for the plot
     placeholder = st.empty()
     
     # Generate the SHAP text plot with custom figsize
-    fig = plt.figure(figsize=(12, 8))
+    fig = plt.figure(figsize=(12, 50))
     shap_plot = shap.plots.text(_shap_values)
     
     # Display the SHAP plot in the placeholder
